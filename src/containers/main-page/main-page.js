8320c5c63ac8705b261669f99cfef51c4583a914
newnoope <artyomstahov33@gmail.com>
2023-05-15 00:42:19 +0300

    date picker

--- src/containers/main-page/main-page.js
+++ src/containers/main-page/main-page.js
@@ -1,14 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import './main-page.sass';
 
 function MainPage() {
-    return(
-        <>
-          <Header page='main' />  
-          <Footer />
-        </>        
-    );
+  const [value, setValue] = React.useState(dayjs());
+
+  return (
+    <>
+      <Header page='main' />
+      <div className="rest-list__wrapper">
+        <div className="rest-list__filter">
+          <label htmlFor="text-input">Текст:</label>
+          <input type="text" id="text-input" placeholder="Введіть текст" />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Оберіть дату"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+              />
+            </LocalizationProvider>
+          <button>Пошук</button>
+        </div>
+        <ul className="rest-list">
+          {/* Здесь будут элементы списка */}
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default MainPage;
