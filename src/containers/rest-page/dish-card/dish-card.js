56e7b58d1b5b1f17ac1e80dd5f54c05837ed4e0e
newnoope <artyomstahov33@gmail.com>
2023-05-18 01:20:59 +0300

    orders and dishes

--- src/containers/rest-page/dish-card/dish-card.js
+++ src/containers/rest-page/dish-card/dish-card.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import {faCircleMinus, faCirclePlus} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { faCircleMinus, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './dish-card.sass';
 
-function DishCard(props) {
-  const { ingredients } = props;
+function DishCard({ dish, handleCountChange, dishCount }) {
+  const { id, image, name, description, ingredients, price } = dish;
 
   const formatData = (item) => {
     const { name, quantity, unit } = item;
@@ -14,42 +14,45 @@ function DishCard(props) {
 
   const formattedList = ingredients.map(formatData).join(', ');
 
-  const [count, setCount] = useState(0);
-
   const handleIncrement = () => {
-    setCount(count + 1);
+    handleCountChange(id, dishCount + 1);
   };
 
   const handleDecrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
+    if (dishCount > 0) {
+      handleCountChange(id, dishCount - 1);
     }
   };
+  
 
   const calculatePrice = () => {
-    if (count === 0) {
-      return props.price;
+    if (dishCount === 0) {
+      return price;
     } else {
-      return props.price * count;
+      return price * dishCount;
     }
   };
-
   return (
     <div className="dish-card__wrapper">
       <div className="dish-card__img">
-        <img src={props.image} alt={`Dish ${props.name}`} />
+        <img src={image} alt={`Dish ${name}`} />
       </div>
-      <div className="dish-card__name">{props.name}</div>
-      <div className="dish-card__descr">{props.description}</div>
+      <div className="dish-card__name">{name}</div>
+      <div className="dish-card__descr">{description}</div>
       <div className="dish-card__ingr">{formattedList}</div>
       <div className="dish-card__price">{calculatePrice()}</div>
       <div className="dish-card__count">
-        <div className="count-btn" onClick={handleDecrement}><FontAwesomeIcon icon={faCircleMinus} /></div>
-        <div className="count-value">{count}</div>
-        <div className="count-btn" onClick={handleIncrement}><FontAwesomeIcon icon={faCirclePlus} /></div>
+        <div className="count-btn" onClick={handleDecrement}>
+          <FontAwesomeIcon icon={faCircleMinus} />
+        </div>
+        <div className="count-value">{dishCount}</div>
+        <div className="count-btn" onClick={handleIncrement}>
+          <FontAwesomeIcon icon={faCirclePlus} />
+        </div>
       </div>
     </div>
   );
 }
+
 
 export default DishCard;
--- src/containers/rest-page/rest-page.js
+++ src/containers/rest-page/rest-page.js
@@ -1,6 +1,8 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import { ExpandMore } from '@mui/icons-material';
 import Header from '../../components/header/header';
 import Carousel from 'react-bootstrap/Carousel';
 import DishCard from './dish-card/dish-card';
@@ -8,53 +10,97 @@ import './rest-page.sass';
 
 function RestPage() {
   const { id } = useParams();
-  const [restaurant, setRestaurant] = useState([]);
+  const [restaurant, setRestaurant] = useState({});
   const [dishes, setDishes] = useState([]);
+  const [total_price, settotal_price] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const restaurantResponse = await axios.get(`http://localhost:3002/restaurant/${id}`);
         setRestaurant(restaurantResponse.data);
-  
+
         const dishResponses = await Promise.all(
           restaurantResponse.data.dishes.map((dishId) => axios.get(`http://localhost:3002/dish/${dishId}`))
         );
-        const dishData = dishResponses.map((res) => res.data);
+        const dishData = dishResponses.map((res) => ({ ...res.data, id: res.data._id, count: 0 }));
+
         setDishes(dishData);
       } catch (error) {
         console.error(error);
       }
     };
-  
+
     fetchData();
   }, [id]);
 
-  // Функция для открытия ссылки на Google Maps
   const openGoogleMaps = () => {
     const address = encodeURIComponent(restaurant.address);
     const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
     window.open(googleMapsUrl, '_blank');
   };
 
-  // Функция для форматирования ссылки почты
   const formatEmail = (email) => {
     const formattedEmail = `mailto:${email}`;
     return formattedEmail;
   };
 
+  const uniqueCategories = [...new Set(dishes.map((dish) => dish.category))];
+
+  const handleOrder = () => {
+    const user = 'your_user_id'; // Замените на фактический id пользователя
+  
+    // Получение текущей даты
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+    const order = {
+      user,
+      dishes: dishes
+        .filter((dish) => dish.count >= 1)
+        .map((dish) => ({ id: dish.id, count: dish.count })),
+      total_price,
+      data: formattedDate // Добавление свойства data с днем заказа
+    };
+  
+    console.log(order); // Вывод объекта заказа в консоль (для тестирования)
+    // Отправка заказа на сервер - добавьте соответствующий код здесь
+  };
+  
+
+  const handleCountChange = (dishId, dishCount) => {
+    setDishes((prevDishes) =>
+      prevDishes.map((prevDish) => {
+        if (prevDish.id === dishId) {
+          return { ...prevDish, count: dishCount };
+        }
+        return prevDish;
+      })
+    );
+  };
+
+
+  useEffect(() => {
+    const total_price = dishes.reduce((total, dish) => {
+      return total + dish.price * dish.count;
+    }, 0);
+    settotal_price(total_price);
+  }, [dishes]);
+
+
+
   return (
     <>
       <Header page='rest' />
       <div className="rest-info__wrapper">
-        <div className="rest-info__name">
-          {restaurant.name}
-        </div>
-        <div className="rest-info__descr">
-          {restaurant.description}
-        </div>
+        <div className="rest-info__name">{restaurant.name}</div>
+        <div className="rest-info__descr">{restaurant.description}</div>
         <div className="rest-info__address">
-          <a href="#" onClick={openGoogleMaps}>{restaurant.address}</a>
+          <a href="#" onClick={openGoogleMaps}>
+            {restaurant.address}
+          </a>
         </div>
         <div className="rest-info__phone">
           <a href={`tel:${restaurant.phone}`}>{restaurant.phone}</a>
@@ -62,40 +108,53 @@ function RestPage() {
         <div className="rest-info__email">
           <a href={formatEmail(restaurant.email)}>{restaurant.email}</a>
         </div>
-        <div className="rest-info__rating">
-          {restaurant.rating}
-        </div>
-        <button className="rest-info__btn">
-          Забронювати стіл
-        </button>
+        <div className="rest-info__rating">{restaurant.rating}</div>
       </div>
       <div className="rest-img-slider__wrapper">
         <Carousel interval={10000}>
-          {restaurant.photos && restaurant.photos.map((photo, index) => (
-            <Carousel.Item key={index}>
-              <img className="rest-img-slider__item" src={`${photo}`} alt={`Photo ${index + 1}`} />
-            </Carousel.Item>
-          ))}
+          {restaurant.photos &&
+            restaurant.photos.map((photo, index) => (
+              <Carousel.Item key={index}>
+                <img className="rest-img-slider__item" src={`${photo}`} alt={`Photo ${index + 1}`} />
+              </Carousel.Item>
+            ))}
         </Carousel>
       </div>
       {dishes.length > 0 && (
-        <div className="dish-cards__wrapper">
-          {dishes.map((dish) => (
-            <DishCard
-              key={dish.id}
-              image={dish.image}
-              name={dish.name}
-              description={dish.description}
-              ingredients={dish.ingredients}
-              price={dish.price}
-            />
+        <div className="dish-cards__wrapper accordion-container">
+          {uniqueCategories.map((category) => (
+            <Accordion key={category}>
+              <AccordionSummary expandIcon={<ExpandMore />} aria-controls={`${category}-content`} id={`${category}-header`}>
+                <Typography>{category}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="dish-cards__wrapper">
+                  {dishes
+                    .filter((dish) => dish.category === category)
+                    .map((dish) => {
+                      const dishCopy = { ...dish };
+                      return (
+                        <DishCard
+                          key={dishCopy.id}
+                          dish={dishCopy}
+                          handleCountChange={handleCountChange}
+                          dishCount={dishCopy.count} // Замените dishCopy.count на dishCount
+                        />
+
+                      );
+                    })}
+                </div>
+              </AccordionDetails>
+            </Accordion>
           ))}
         </div>
       )}
-
+      <button className="rest-info__btn" onClick={handleOrder}>
+        Order
+      </button>
+      <div className="rest-info__total-price">Total Price: {total_price}</div>
     </>
   );
-
 }
 
 export default RestPage;
